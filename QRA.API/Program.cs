12bec1f8bb4ce8fe9a0d96277b1c71cbbc8a4b10
juliandarivas <julian.rivas.sanchez@outlook.com>
2018-08-12 18:15:51 -0500

    Se elimina llamado de DbInitializer

--- QRA.API/Program.cs
+++ QRA.API/Program.cs
@@ -12,23 +12,7 @@ namespace QRA.API
     {
         public static void Main(string[] args)
         {
-            var host = BuildWebHost(args);
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<QRAContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred initializing the database.");
-                }
-            }
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
