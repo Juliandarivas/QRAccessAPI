bac8a4964bf28bf1b84e7754c174ec6f43086924
juliandarivas <juliandarivas@hotmail.com>
2018-08-27 23:53:51 -0500

    Se añaden entidades de Codigo y CodigoCuenta

---
+++ QRA.Entities/Content/Codigo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QRA.Entities.Content
+{   
+    public class Codigo
+    {
+        public int Id { get; set; }
+        public string Identificador { get; set; }
+        public string Token { get; set; }
+
+
+        public virtual ICollection<CodigoCuenta> CodigosCuentas { get; set; }
+    }
+}
